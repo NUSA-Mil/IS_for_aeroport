38eda61a539a7f2ac7aa2890f43016fea62929f0
NUSA-Mil <sikorskijigor36@gmail.com>
2024-12-02 16:49:07 +0300

    Add files via upload

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-from space import view_available_spaces, book_space, view_booking_history, filter_spaces, sort_spaces, format_spaces, cancel_booking, spaces
+from space import view_available_spaces, book_space as global_book_space, view_booking_history, filter_spaces, sort_spaces, format_spaces, cancel_booking as global_cancel_booking, spaces
 from utilis import safe_input
 
 def user_menu(current_user):
@@ -6,30 +6,29 @@ def user_menu(current_user):
         available_spaces = view_available_spaces()
         format_spaces(available_spaces)
 
-    def book_space():
+    def book_space_action():
         space_id = int(safe_input("Введите ID помещения: ", lambda x: x.isdigit(), "Пожалуйста, введите число."))
-        book_space(current_user, space_id)
+        global_book_space(current_user, space_id)
 
     def view_history():
         view_booking_history(current_user)
 
-    def sort_spaces():
+    def sort_spaces_action():
         key = safe_input("Введите критерий сортировки (price, rating): ", lambda x: x in ['price', 'rating'], "Пожалуйста, введите 'price' или 'rating'.")
         reverse = safe_input("Сортировать по убыванию? (y/n): ", lambda x: x in ['y', 'n'], "Пожалуйста, введите 'y' или 'n'.") == 'y'
         sorted_spaces = sort_spaces(lambda space: space[key], reverse)
         format_spaces(sorted_spaces)
 
-    def filter_spaces():
+    def filter_spaces_action():
         min_price = int(safe_input("Введите минимальную цену: ", lambda x: x.isdigit(), "Пожалуйста, введите число."))
         max_price = int(safe_input("Введите максимальную цену: ", lambda x: x.isdigit(), "Пожалуйста, введите число."))
         criteria = lambda space: min_price <= space['price'] <= max_price
         filtered_spaces = filter_spaces(criteria)
         format_spaces(filtered_spaces)
 
-
-    def cancel_booking():
+    def cancel_booking_action():
         space_id = int(safe_input("Введите ID помещения: ", lambda x: x.isdigit(), "Пожалуйста, введите число."))
-        cancel_booking(current_user, space_id)
+        global_cancel_booking(current_user, space_id)
 
     def logout():
         nonlocal running
@@ -37,11 +36,11 @@ def user_menu(current_user):
 
     actions = {
         1: view_spaces,
-        2: book_space,
+        2: book_space_action,
         3: view_history,
-        4: sort_spaces,
-        5: filter_spaces,
-        6: cancel_booking,
+        4: sort_spaces_action,
+        5: filter_spaces_action,
+        6: cancel_booking_action,
         7: logout
     }
 
